24c5952eaf43bac1160fa43d1297690bfc4a5e3f
Artem Tselikov <ar.tselikov@gmail.com>
2017-07-14 13:30:44 -0400

    added pods file

---
+++ simpleContacts/AddContactViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddContactViewController.swift
+//  simpleContacts
+//
+//  Created by Artem Tselikov on 2017-07-14.
+//  Copyright © 2017 Artem Tselikov. All rights reserved.
+//
+
+import UIKit
+
+class AddContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
